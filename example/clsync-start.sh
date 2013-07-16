6687943743eda40eae449c511bac626108ec1391
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2013-07-16 19:18:06 +0400

    bugfix in example script

--- example/clsync-start.sh
+++ example/clsync-start.sh
@@ -3,5 +3,11 @@
 make -C .. all
 
 mkdir -p testdir/{from,to,listdir}
+
+cat > rules <<EOF
+-d/\\.git\$
++*.*
+EOF
+
 ../clsync -d ./testdir/listdir -w 2 -p -t 5 ./testdir/from ./clsync-actionscript.sh rules
 
