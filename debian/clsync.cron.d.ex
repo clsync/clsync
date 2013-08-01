3beb370bf25a301c72d2ef58c048891d0e0b7c0d
Artyom A Anikeev <anikeev@ut.mephi.ru>
2013-08-01 18:19:04 +0400

    manpage was been fixed conforming to lintian requirements, and example files were been removed

--- debian/clsync.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the clsync package
-#
-0 4	* * *	root	[ -x /usr/bin/clsync_maintenance ] && /usr/bin/clsync_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(clsync):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="clsync" command="/usr/bin/clsync"
