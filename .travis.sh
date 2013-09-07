8091b0b4d8ccfbe22f054b434821c4e13f39e13b
Andrew Savchenko <bircoph@gmail.com>
2013-09-07 12:34:40 +0400

    Add capabilities to the travis test.

--- .travis.sh
+++ .travis.sh
@@ -9,14 +9,16 @@ autoreconf -if
 # world empty values should be the same as --enable/--with default
 # values, but they may not due to bugs.
 
-for a1 in "" "--enable-cluster" "--disable-cluster"; do
-    arg[0]="$a1"
-for a2 in "" "--enable-debug" "--disable-debug"; do
-    arg[1]="$a2"
-for a3 in "" "--enable-paranoid=0" "--enable-paranoid=1" "--enable-paranoid=2" ; do
-    arg[2]="$a3"
+for a0 in "" "--enable-cluster" "--disable-cluster"; do
+    arg[0]="$a0"
+for a1 in "" "--enable-debug" "--disable-debug"; do
+    arg[1]="$a1"
+for a2 in "" "--enable-paranoid=0" "--enable-paranoid=1" "--enable-paranoid=2" ; do
+    arg[2]="$a2"
+for a3 in "" "--with-capabilities" "--without-capabilities"; do
+    arg[3]="$a3"
 for a4 in "" "--with-mhash" "--without-mhash"; do
-    arg[3]="$a4"
+    arg[4]="$a4"
 
     make clean
     echo ">>> Testing with \"${arg[@]}\""
@@ -26,6 +28,7 @@ for a4 in "" "--with-mhash" "--without-mhash"; do
         exit 1
     }
 
+done
 done
 done
 done
