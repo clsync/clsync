6b3aa6714a75629e19ff968c969ecec870f50150
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2013-09-26 14:53:13 +0400

    Fixed production examples installing in Makefile.am and removed obsolete examples

--- examples/clsync-start-production.sh
+++
@@ -1,5 +0,0 @@
-
-ulimit -n 65536
-
-clsync -K example-production -Y -z /opt/backup_engine/clsync.pid -F -M rsyncshell -w15 -t5 -L /opt/backup_engine/list -W "$(readlink -f /srv/lxc)" -S /opt/backup_engine/clsync-synchandler-production.sh -R /opt/backup_engine/clsync.rules &
-
