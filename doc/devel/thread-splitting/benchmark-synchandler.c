aa2767df70dd4abdf41a95ec4384133dc8834607
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-08-18 19:23:23 +0400

    Fixed benchmark-synchandler.c

--- doc/devel/thread-splitting/benchmark-synchandler.c
+++ doc/devel/thread-splitting/benchmark-synchandler.c
@@ -12,27 +12,20 @@
 
 struct ctx *ctx_p         = NULL;
 
-// Optional function, you can erase it.
 int clsyncapi_init(struct ctx *_ctx_p, struct indexes *_indexes_p) {
-	debug(1, "Hello world!");
-
 	ctx_p = _ctx_p;
-
 	return 0;
 }
 
 int clsyncapi_rsync(const char *inclistfile, const char *exclistfile) {
-	debug(1, "clsyncapi_rsync()");
 	return 0;
 }
 
 int clsyncapi_sync(int n, api_eventinfo_t *ei) {
-	debug(1, "clsyncapi_sync(): n == %i", n);
 	return 0;
 }
 
 int clsyncapi_deinit() {
-	debug(1, "Goodbye cruel world!");
 	return 0;
 }
 
